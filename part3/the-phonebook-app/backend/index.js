ee1add31c8aa30d3d7827a57f0d5787263770601
Pratik Sonthaliya <psonthal@fastenal.com>
2024-02-26 18:00:59 +0530

    part 3a completed & assignments till 3.8

---
+++ part3/the-phonebook-app/backend/index.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(req.body),
+    ].join(" ");
+  })
+);
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.use(express.json());
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.status(200).json(person);
+  }
+  res.status(404).send("Person not there");
+});
+
+app.delete("/api/perons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const newPersons = persons.filter((person) => person.id !== id);
+  persons = newPersons;
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return res.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 10000000),
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
+});
+
+app.get("/info", (req, res) => {
+  res.send(`
+        <h4>Phonebook has info for ${persons.length} people</h4>
+        <h4>${Date()}</h4>
+    `);
+});
+
+app.listen(3001);
