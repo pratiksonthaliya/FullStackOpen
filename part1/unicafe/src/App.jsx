25665aac0e877f8a8c9ff369b91e384f3a900086
Pratik Sonthaliya <pratiksonthaliya@gmail.com>
2024-02-27 20:43:01 +0530

    part 0 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './App.css'
 
 const Button = (props) => {
   console.log(props);
@@ -19,23 +20,21 @@ const StatisticsLine = ({text, value}) => {
 }
 
 const Statistics = ({good, bad, neutral}) => {
-  if(good+bad+neutral > 0){
-    return(
+  return(
+    good+bad+neutral > 0 ?
       <div>
         <h2>Statistics</h2>
         <StatisticsLine text="Good:" value={good}/>
         <StatisticsLine text="Neutral:" value={neutral}/>
         <StatisticsLine text="Bad:" value={bad}/>
         <StatisticsLine text="All:" value={good+neutral+bad}/>
-        <StatisticsLine text="Positive Percentage:" value={good*100/(good+neutral+bad) }/>
-      </div>  
-    )
-  }
-  else return(
-    <div>
-      <h2>Statistics</h2>
-      <h3>No feedback given</h3>
-    </div>
+        <StatisticsLine text="Positive Percentage:" value={(good*100/(good+neutral+bad)).toFixed(2)}/>
+      </div> 
+    :
+      <div>
+        <h2>Statistics</h2>
+        <h3>No feedback given</h3>
+      </div>
   )
 }
 
@@ -45,14 +44,16 @@ function App() {
   const [bad, setBad] = useState(0)
 
   return (
-    <>
-      <h2>Give Feedback</h2>
-      <Button onClick={()=>setGood(good+1)} text="Good"/>
-      <Button onClick={()=>setNeutral(neutral+1)} text="Neutral"/>
-      <Button onClick={()=>setBad(bad+1)} text="Bad"/>
-      
-      <Statistics good={good} bad={bad} neutral={neutral}/>
-    </>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <div>
+        <h2>Give Feedback</h2>
+        <Button onClick={()=>setGood(good+1)} text="Good"/>
+        <Button onClick={()=>setNeutral(neutral+1)} text="Neutral"/>
+        <Button onClick={()=>setBad(bad+1)} text="Bad"/>
+        
+        <Statistics good={good} bad={bad} neutral={neutral}/>
+      </div>
+    </div>
   )
 }
 
